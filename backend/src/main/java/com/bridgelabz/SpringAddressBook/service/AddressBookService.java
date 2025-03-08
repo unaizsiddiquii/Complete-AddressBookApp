f11f9c0ca9086d45de607d9780d46e055c17f5a4
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-03-08 22:43:02 +0530

    Unaiz- Add Spring backend part of address book app

---
+++ backend/src/main/java/com/bridgelabz/SpringAddressBook/service/AddressBookService.java
@@ -0,0 +1,58 @@
+package com.bridgelabz.SpringAddressBook.service;
+
+
+
+
+
+import com.bridgelabz.SpringAddressBook.dto.AddressBookDTO;
+import com.bridgelabz.SpringAddressBook.model.AddressBook;
+import com.bridgelabz.SpringAddressBook.repository.AddressBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AddressBookService {
+
+    @Autowired
+    private AddressBookRepository repository;
+
+    public AddressBook saveEntry(AddressBookDTO addressBookDTO) {
+        AddressBook addressBook = new AddressBook();
+        addressBook.setName(addressBookDTO.getName());
+        addressBook.setPhoneNumber(addressBookDTO.getPhoneNumber());
+        addressBook.setEmail(addressBookDTO.getEmail());
+        addressBook.setAddress(addressBookDTO.getAddress());
+        return repository.save(addressBook);
+    }
+
+
+
+
+    public List<AddressBook> getAllEntries() {
+        return repository.findAll();
+    }
+
+    public Optional<AddressBook> getEntryById(Long id) {
+        return repository.findById(id);
+    }
+
+
+    public AddressBook updateEntry(Long id, AddressBookDTO newEntryDTO) {
+        return repository.findById(id)
+                .map(entry -> {
+                    entry.setName(newEntryDTO.getName());
+                    entry.setPhoneNumber(newEntryDTO.getPhoneNumber());
+                    entry.setEmail(newEntryDTO.getEmail());
+                    entry.setAddress(newEntryDTO.getAddress());
+                    return repository.save(entry);
+                })
+                .orElseThrow(() -> new RuntimeException("Entry not found"));
+    }
+
+    public void deleteEntry(Long id) {
+        repository.deleteById(id);
+    }
+}
